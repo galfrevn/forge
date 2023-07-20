f6a430289c4ec721fd629b280b0bccae22108571
Valentin G <galfre.vn@gmail.com>
2023-07-20 01:04:18 -0300

    feat: removing javascript files of sample sst template

--- examples/fincast/packages/core/src/event.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.event = void 0;
-const event_bus_1 = require("sst/node/event-bus");
-exports.event = (0, event_bus_1.createEventBuilder)({
-    bus: "bus",
-});
