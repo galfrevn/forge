4e53787e0b87b579a7f7403febc4ea6bf8e188d0
Valentin G <galfre.vn@gmail.com>
2023-07-21 19:31:06 -0300

    build: ignoring typescript errors for documentation

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com'],
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default withContentlayer(nextConfig);
